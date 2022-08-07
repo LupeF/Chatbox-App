40b1e64a2cb78abf34af5836119f385acbda3203
LupeF <46634202+LupeF@users.noreply.github.com>
2022-08-07 15:24:50 -0700

    add event listeners to li items

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 const messageWindow = document.querySelector('.message-window');
 const message = document.getElementById('input');
 const button = document.getElementById('button');
+const nameDiv = document.getElementById('name-container');
+// const li = document.getElementsByTagName('li');
+const li = document.querySelectorAll('li');
 
+//************************************* */
+// Assigns event listeners to li elements
+//************************************* */
+for(let i=0; i<li.length; i++){
+    li[i].addEventListener('click', () => {
+       nameDiv.classList.add('name-styles'); 
+       nameDiv.innerHTML = `
+        <h1>${li[i].textContent}</h1>
+       `
+    })
+}
+
+
+//*******************************/
+// Appends Messages To The Window
+//******************************/
 button.addEventListener('click', () => {
     const div = document.createElement('div');
     messageWindow.append(div);
