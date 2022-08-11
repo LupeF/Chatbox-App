9259a48119c59165fbfeb5e6c5a6be89921fe0f1
LupeF <46634202+LupeF@users.noreply.github.com>
2022-08-11 13:09:13 -0700

    add over flow styles and utilities partial

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,12 @@ const messageWindow = document.querySelector('.message-window');
 const message = document.getElementById('input');
 const button = document.getElementById('button');
 const nameDiv = document.getElementById('name-container');
-// const li = document.querySelectorAll('li');
+const li = document.querySelectorAll('li');
 const nav = document.querySelector('nav');
 
 //************************************* */
 // Assigns event listeners to li elements
 //************************************* */
-// for(let i=0; i<li.length; i++){
-//     li[i].addEventListener('click', () => {
-//        nameDiv.classList.add('selected-name'); 
-//        nameDiv.innerHTML = `
-//         <h1>${li[i].textContent}</h1>
-//         `
-//     })
-// };
-
 nav.addEventListener('click', (e) => {
     if(e.target.tagName === "LI")
     nameDiv.classList.add('selected-name');
